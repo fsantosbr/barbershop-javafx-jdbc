d46c30bd934cbf8c0bdf5fb87b7be1b9c053f08e
Fabio Alves <fsantosinfo@gmail.com>
2020-09-08 10:59:14 -0300

    Added the MainView.fxml

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,35 +1,39 @@
 package application;
 	
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.entities.Agenda;
 import model.entities.Barber;
 import model.entities.Client;
 import model.entities.Person;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load();
+			
+			Scene mainScene = new Scene(parent);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Barber Shop - By @fsantosbr");
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] args) {
-		//launch(args);
+		launch(args);
 		
 		System.out.println("New client? New Barber?");
 		System.out.println("Inform who you are!!");
